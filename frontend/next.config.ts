2912985d00eebdf525d3d0251cd5f78ee500fcf8
Codefish <codewithfish@gmail.com>
2025-04-30 12:25:15 +0900

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,33 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    // swcMinify: true,
-    images: {
-        domains: ['localhost', 're-cord.s3.ap-northeast-2.amazonaws.com'],
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 're-cord.s3.ap-northeast-2.amazonaws.com',
-                pathname: '/**',
-            },
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '8090',
-                pathname: '/**',
-            },
-        ],
-    },
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/home',
-                permanent: true,
-            },
-        ]
-    },
-    
+  reactStrictMode: true,
+  // 빌드 시 ESLint 검사 비활성화
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // 빌드 시 TypeScript 오류 무시
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // 이미지 관련 설정
+  images: {
+    domains: ['localhost', 're-cord.s3.ap-northeast-2.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 're-cord.s3.ap-northeast-2.amazonaws.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8090',
+        pathname: '/**',
+      },
+    ],
+  },
+  // 리디렉션 설정
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
